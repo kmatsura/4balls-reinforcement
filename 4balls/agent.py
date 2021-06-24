8cc1b675014b958e7972815e0156e563f70c44dd
kmatsuuraHMC <bcdearth@gmail.com>
2021-06-25 03:48:53 +0900

    Add:とりあえずランダムに置くだけの機能を追加

--- 4balls/agent.py
+++ 4balls/agent.py
@@ -1,8 +1,15 @@
+import random
+
+
 class Agent:
-    def __init__(self, h, w):
+    def __init__(self, w, h, c, mode="reinforce"):
+        """
+        mode = reinforce or random
+        """
         self.h = h
         self.w = w
-        self.Q = 
+        self.c = c
+        self.mode = mode
         pass
 
     def load_param(self, param):
@@ -23,16 +30,52 @@ class Agent:
         """
         pass
 
-    def calc_score(self):
+    def calc_score_and_col(self, game):
         """
         盤面の評価値を計算する。ルールで決まったら期待報酬の計算はしない。
         """
+        point = self.calc_rule_score(game)
+        if point:
+            return point
+        elif self.mode == "random":
+            cand = game.cand
+            col = random.choice(cand)
+            return col
+
         pass
 
-    def calc_rule_score(self):
+    def calc_rule_score(self, board):
         """
         ルールで評価値をチェックする。
+        1. 自分がリーチのときは、4つ目を置く。
+        2. 相手に3マスの直線があった場合は、リーチなので、(塞げる場合は)片方を塞ぐ。
+        3. 相手に2マスの直線があり、その後一つ飛ばしで両端が空いている場合は、片方を塞がなければ負けてしまうので、（塞げる場合)片方を塞ぐ。
+        4.  相手に2マスの直線があり、両端が空いている場合は、片方を塞がなければ負けてしまうので、（塞げる場合)片方を塞ぐ。
         """
+        p1 = self.check_rule1(board)
+        if p1:
+            return p1
+        p2 = self.check_rule2(board)
+        if p2:
+            return p2
+        p3 = self.check_rule3(board)
+        if p3:
+            return p3
+        p4 = self.check_rule4(board)
+        if p4:
+            return p4
+        return None
+
+    def check_rule1(self, board):
+        pass
+
+    def check_rule2(self, board):
+        pass
+
+    def check_rule3(self, board):
+        pass
+
+    def check_rule4(self, board):
         pass
 
     def calc_rein_score(self):
