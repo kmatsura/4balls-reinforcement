f3271eb2cb12c04bfce34205440c9fe00b68b36e
kmatsuuraHMC <bcdearth@gmail.com>
2021-06-26 01:08:05 +0900

    Update: 表示する情報を追加

--- 4balls/demo.py
+++ 4balls/demo.py
@@ -12,14 +12,21 @@ def main():
     n = 1
     while(fb.finish == 0 and fb.cand):
         print("===turn {}====".format(n))
+        print("computer")
         col = agent1.calc_score_and_col(fb)
         fb.put_ball(col, agent1.c)
         pprint(fb.board)
+        if fb.finish != 0:
+            print("Computer wins!!")
+            break
         print("ボールを置く列を指定してください。")
+        print("player")
         col_user = int(input())
         fb.put_ball(col_user, 2)
         pprint(fb.board)
         n += 1
+        if fb.finish != 0:
+            print("You win!!")
 
 
 
